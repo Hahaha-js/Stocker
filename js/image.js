e0868878163e010da890f9f63ab475d4243469be
Hahaha-js <jin_84269@naver.com>
2021-01-05 00:32:36 +0900

    Add modal events & styles

--- js/image.js
+++ js/image.js
@@ -1,41 +1,69 @@
 
+    function setModal(obj) {
+      const modal_inner = document.getElementById('modal_inner');
 
-var container = document.querySelector('#container');
-var pageElement = document.querySelector('#page');
-
-function Data() {
-  var pageVal = pageElement.value;
-  if (pageVal <= 0) {
-    alert('페이지 값을 확인해 주세요');
-  } else {
-    container.innerHTML = '';          // 이미지 누적 안되게 가져오는 법           
-    getData(pageVal);
-  }
-}
-
-function getData() {
-  var pageVal = pageElement.value;
-  var url = `https://picsum.photos/v2/list?page=${pageVal}&limit=18`;
-  axios.get(url).then(function (res) {
-    console.log(res);
-    if (res.status === 200) { //통신이 잘됐다 응답이 왔다.라는 뜻
-      res.data.forEach(function (item) {
-        makeItem(item);
-      });
+      modal_inner.innerHTML = `
+              <img src="${obj.file_path}" alt="image -${obj.title}">
+              <h2>${obj.title}</h2>
+              <strong>${obj.author_name}</strong>
+              <p>${obj.description}</p>
+              <small>${obj.regdate}</small>
+    
+            `;
     }
-  });
-}
+    // id 모달인 엘리먼트를 엡솔루트를 줘서 바디태그를 다 덮어버리자
+    // 릴레이티브를 받는쪽을 정해보자
+    // modal 은 dispaly: none
 
-function makeItem(item) {
-  var modUrl = `https://picsum.photos/id/${item.id}/300`;
-  var img = document.createElement('img');
-  img.src = modUrl;
-  container.append(img);
+    function hideModal(e) {
+      if(e.target == modal) {
+        modal.classList.remove('bg')
+      }
+    }
 
-}
-
-
-
-
+    function showModal() {
+      modal.classList.add('bg');
+    }
 
 
+    function createE(obj) {
+      const el = document.createElement('div');
+      el.className = 'item';
+      el.innerHTML = `
+          <img src="${obj.file_path}" alt="image -${obj.title}">
+          <div>
+            <strong>${obj.title}</strong>
+            <span>${obj.author_name}</span>
+          </div>
+            `;
+
+      // el.onclick = setModal;
+
+      el.onclick = function () {
+        showModal();
+        setModal(obj);
+      }
+
+      return el;
+    }
+
+    function init() {
+
+      var container = document.querySelector('#container');
+
+
+      const url = './data.json';
+      axios.get(url).then(function (res) {
+
+        res.data.forEach(function (obj) {
+          container.append(createE(obj));
+
+        })
+
+      })
+      
+      const modal = document.getElementById('modal');
+      modal.onclick = hideModal;
+    }
+
+    init();
